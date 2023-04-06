cf2ed26c522325fd7469f0f56b320e1e0c967610
Nguyễn Văn An <nva6112002@gmail.com>
2023-04-06 11:01:40 +0700

    Login, register, profile, update profile

--- FindYourDoctor/app/src/main/java/com/hcmute/findyourdoctor/Activity/RegisterActivity.java
+++ FindYourDoctor/app/src/main/java/com/hcmute/findyourdoctor/Activity/RegisterActivity.java
@@ -1,9 +1,12 @@
 package com.hcmute.findyourdoctor.Activity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
@@ -14,8 +17,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.gson.JsonObject;
 import com.hcmute.findyourdoctor.Api.AuthApiService;
 import com.hcmute.findyourdoctor.Api.RetrofitClient;
+import com.hcmute.findyourdoctor.Fragment.ProfileFragment;
 import com.hcmute.findyourdoctor.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
 
 import retrofit2.Call;
@@ -23,7 +29,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class RegisterActivity extends AppCompatActivity {
-    EditText tvName, tvEmail, tvPassword, tvRePwd;
+    EditText edtName, edtEmail, edtPassword, edtRePwd, edtPhone, edtBirthdate;
     TextView btnSignup, tvLogin;
     RadioButton radioPolicy;
     AuthApiService authApiService;
@@ -40,7 +46,7 @@ public class RegisterActivity extends AppCompatActivity {
                 {
                     authApiService = RetrofitClient.getRetrofit().create(AuthApiService.class);
                     HashMap<String, String> body = new HashMap<>();
-                    body.put("email", tvEmail.getText().toString().trim());
+                    body.put("email", edtEmail.getText().toString().trim());
                     authApiService.sendCodeToEmail(body).enqueue(new Callback<JsonObject>() {
                         @Override
                         public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
@@ -52,9 +58,12 @@ public class RegisterActivity extends AppCompatActivity {
                                 code = res.get("result").toString();
 
                                 Intent intent = new Intent(RegisterActivity.this, VerifyCodeActivity.class);
-                                intent.putExtra("name", tvName.getText().toString());
-                                intent.putExtra("email", tvEmail.getText().toString());
-                                intent.putExtra("password", tvPassword.getText().toString());
+                                intent.putExtra("name", edtName.getText().toString());
+                                intent.putExtra("email", edtEmail.getText().toString());
+                                intent.putExtra("phone", edtPhone.getText().toString());
+                                Log.d("nva", edtBirthdate.getText().toString());
+                                intent.putExtra("birthDate", edtBirthdate.getText().toString());
+                                intent.putExtra("password", edtPassword.getText().toString());
                                 intent.putExtra("code", code);
 
                                 startActivity(intent);
@@ -79,22 +88,24 @@ public class RegisterActivity extends AppCompatActivity {
         });
 
     }
+
     private boolean checkText() {
-        if(TextUtils.isEmpty(tvName.getText()) || TextUtils.isEmpty(tvEmail.getText()) || 
-                TextUtils.isEmpty(tvPassword.getText()) || TextUtils.isEmpty(tvRePwd.getText())) {
+        if(TextUtils.isEmpty(edtName.getText()) || TextUtils.isEmpty(edtEmail.getText()) || 
+                TextUtils.isEmpty(edtPassword.getText()) || TextUtils.isEmpty(edtRePwd.getText()) ||
+                TextUtils.isEmpty(edtPhone.getText()) || TextUtils.isEmpty(edtBirthdate.getText())) {
             Toast.makeText(this, "Information cannot be left blank", Toast.LENGTH_SHORT).show();
             return false;
         }
-        if (!TextUtils.equals(tvPassword.getText(), tvRePwd.getText()))
+        if (!TextUtils.equals(edtPassword.getText(), edtRePwd.getText()))
         {
             Toast.makeText(this, "Invalid confirmation password", Toast.LENGTH_SHORT).show();
-            tvRePwd.setText("");
-            tvRePwd.requestFocus();
+            edtRePwd.setText("");
+            edtRePwd.requestFocus();
             return false;
         }
-        if(!isEmail(tvEmail.getText().toString())) {
+        if(!isEmail(edtEmail.getText().toString())) {
             Toast.makeText(this, "Invalid email", Toast.LENGTH_SHORT).show();
-            tvEmail.requestFocus();
+            edtEmail.requestFocus();
             return false;
         }
         if(!radioPolicy.isChecked()) {
@@ -103,17 +114,47 @@ public class RegisterActivity extends AppCompatActivity {
         }
         return true;
     }
+    // TODO: code here
+    public static boolean isValidPhoneNumber(String phoneNumber) {
+        String regex = "((\\+84)|(84)|0)9[01]\\d{7}";
+        return phoneNumber.matches(regex);
+    }
     public boolean isEmail(String text) {
         String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
         return text.matches(emailPattern);
     }
     private void init() {
-        tvName = findViewById(R.id.edt_name_reg);
-        tvEmail = findViewById(R.id.et_email_reg);
-        tvPassword = findViewById(R.id.edt_password_reg);
-        tvRePwd = findViewById(R.id.tv_re_pwd_reg);
+        edtName = findViewById(R.id.edt_name_reg);
+        edtEmail = findViewById(R.id.edt_email_reg);
+        edtPassword = findViewById(R.id.edt_password_reg);
+        edtBirthdate = findViewById(R.id.edt_birthdate_reg);
+        edtRePwd = findViewById(R.id.tv_re_pwd_reg);
+        edtPhone = findViewById(R.id.edt_phone_reg);
         btnSignup = findViewById(R.id.btn_signup);
         radioPolicy = findViewById(R.id.radio_poplicy);
         tvLogin = (TextView) findViewById(R.id.tv_register_log);
+
+        edtBirthdate.setOnClickListener(new View.OnClickListener() {
+            Calendar calendar = Calendar.getInstance();
+            final int year = calendar.get(Calendar.YEAR);
+            final int month = calendar.get(Calendar.MONTH);
+            final int day = calendar.get(Calendar.DAY_OF_MONTH);
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(RegisterActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                        i1 = i1 + 1;
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.set(i, i1-1, i2);
+                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                        String formattedDate = sdf.format(calendar.getTime());
+                        edtBirthdate.setText(formattedDate);
+                    }
+                }, year, month, day);
+
+                datePickerDialog.show();
+            }
+        });
     }
 }
--- backend/src/routers/patient.route.js
+++ backend/src/routers/patient.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { bookingAppointment, updateProfile, updateAvatar } from "../controllers/patient.controller.js"
+import { bookingAppointment, updateProfile, updateAvatar, getProfileById } from "../controllers/patient.controller.js"
 
 import multer from 'multer';
 const router = express.Router();
@@ -8,6 +8,7 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
 router.post('/booking/:bookingId', bookingAppointment);
-router.put('/update-profile/:patientId', updateProfile)
+router.put('/update-profile/:patientId', upload.single('avatar'), updateProfile)
 router.patch('/update-avatar/:patientId', upload.single('avatar'), updateAvatar)
+router.get('/profile/:patientId', getProfileById)
 export default router;
